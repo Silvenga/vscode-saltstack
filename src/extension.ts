2514e6d0950486bdbbd05d8dcaf24c9e7d3775f5
Mark Lopez <m@silvenga.com>
2017-05-03 19:52:16 -0500

    Completed project setup.

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+'use strict';
+import { ExtensionContext, commands, languages, TextDocument, Range } from 'vscode';
+
+export async function activate(context: ExtensionContext) {
+
+    console.log('SLS support now active!');
+
+}
+
+export function deactivate() {
+}
