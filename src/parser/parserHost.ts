a7a4549e88f090f6434aeedbd3c641b6a101331a
Mark Lopez <m@silvenga.com>
2017-05-07 23:11:44 -0500

    Prototyped basic parsing of state declarations.

--- src/parser/parserHost.ts
+++ src/parser/parserHost.ts
@@ -1,16 +1,113 @@
-import * as yaml from "js-yaml"
+import * as yaml from "yaml-ast-parser"
+import { YAMLDocument, YAMLNode, YAMLScalar, YAMLMapping, YamlMap, YAMLSequence, YAMLAnchorReference, Kind } from "yaml-ast-parser"
+
+//     SCALAR = 0,
+//     MAPPING = 1,
+//     MAP = 2,
+//     SEQ = 3,
+//     ANCHOR_REF = 4,
+//     INCLUDE_REF = 5,
+
+const LineBreakRegex = /\r?\n/;
 
 export class ParserHost {
 
     public stripDocument(document: string): string {
-        var jinjaDirectives = /\{[#%][\w\W]*?[#%]\}/g;
+        let jinjaDirectives = /\{[#%][\w\W]*?[#%]\}/g;
         return document.replace(jinjaDirectives,
             // Find all Jinja directives, then replace all non-new lines with empty
             x => x.replace(/./g, ""));
     }
 
-    public getYaml(document: string): any {
-        var doc = yaml.safeLoad(document, yaml.JSON_SCHEMA);
-        return doc;
+    public getYaml(document: string) {
+        return yaml.safeLoad(document, {}) as YamlMap;
     }
+
+    public getMap(document: string): StateFile {
+
+        let strippedDocument = this.stripDocument(document);
+
+        var root = this.getYaml(document);
+        let file = new StateFile();
+        file.startIndex = root.startPosition;
+        file.endIndex = root.endPosition;
+
+        for (let branch of root.mappings) {
+            let declaration = this.mapDeclaration(branch);
+            file.declarations.push(declaration);
+        }
+
+        return file;
+    }
+
+    private mapDeclaration(root: YAMLMapping): StateDeclaration {
+        let declaration = new StateDeclaration();
+        declaration.startIndex = root.key.startPosition;
+        declaration.endIndex = root.key.endPosition;
+        declaration.id = root.key.value;
+
+        // TODO can value be a scalar?     
+        if (root.value == null) {
+            return declaration;
+        }
+        for (let map of root.value.mappings) {
+            let func = this.mapFunction(map);
+            declaration.functions.push(func);
+        }
+
+        return declaration;
+    }
+
+    private mapFunction(root: YAMLMapping): StateFunction {
+        let func = new StateFunction();
+        func.name = root.key.value;
+        func.startIndex = root.key.startPosition;
+        func.endIndex = root.key.endPosition;
+
+        let list = root.value as YAMLSequence;
+        if (list == null) {
+            return func;
+        }
+        for (let item of list.items) {
+            let argument = this.mapArgument(item as YamlMap);
+            func.arguments.push(argument);
+        }
+
+        return func;
+    }
+
+    private mapArgument(root: YamlMap): StateFunctionArgument {
+        let argument = new StateFunctionArgument();
+
+        argument.name = root.mappings[0].key.value;
+        argument.value = root.mappings[0].value.value;
+        argument.startIndex = root.startPosition;
+        argument.endIndex = root.endPosition;
+
+        return argument;
+    }
+}
+
+export class StateNode {
+    startIndex: number;
+    endIndex: number;
+}
+
+export class StateFile extends StateNode {
+    public declarations: StateDeclaration[] = new Array<StateDeclaration>();
+}
+
+export class StateDeclaration extends StateNode {
+    public id: string;
+    public functions: StateFunction[] = new Array<StateFunction>();
+}
+
+export class StateFunction extends StateNode {
+    public name: string;
+    public arguments: StateFunctionArgument[] = new Array<StateFunctionArgument>();
+}
+
+export class StateFunctionArgument extends StateNode {
+    public name: string;
+    public value: string;
 }
---
+++ test/parser/documentMapTests.ts
@@ -0,0 +1,98 @@
+import test from 'ava';
+import { expect } from 'chai';
+import { ParserHost } from "../../src/parser/parserHost"
+
+test('Can parse single declaration', t => {
+
+    let host = new ParserHost();
+    const document =
+        `
+/etc/postfix:
+  file.directory:
+    - user: root
+    - group: root
+    - dir_mode: 755
+    - file_mode: 644
+    - makedirs: True
+`;
+
+    // Act
+    let result = host.getMap(document);
+
+    // Assert
+    t.is(result.declarations.length, 1);
+    t.is(result.declarations[0].startIndex, 1);
+    t.is(result.declarations[0].endIndex, 13);
+    t.is(result.declarations[0].id, "/etc/postfix");
+});
+
+test('Can parse multiple declarations', t => {
+
+    let host = new ParserHost();
+    const document =
+        `
+test1:
+  file.directory:
+test2:
+  file.directory:
+`;
+
+    // Act
+    let result = host.getMap(document);
+
+    // Assert
+    t.is(result.declarations.length, 2);
+    t.deepEqual(result.declarations.map(x => x.id), ["test1", "test2"]);
+});
+
+test('Can parse function.', t => {
+
+    let host = new ParserHost();
+    const document =
+        `
+/etc/postfix:
+  file.directory:
+    - user: root
+    - group: root
+    - dir_mode: 755
+    - file_mode: 644
+    - makedirs: True
+`;
+
+    // Act
+    let result = host.getMap(document);
+
+    // Assert
+    t.is(result.declarations[0].functions.length, 1);
+    t.is(result.declarations[0].functions[0].name, "file.directory");
+    t.is(result.declarations[0].functions[0].startIndex, 17);
+    t.is(result.declarations[0].functions[0].endIndex, 31);
+});
+
+
+
+test('Can parse argument.', t => {
+
+    let host = new ParserHost();
+    const document =
+        `
+/etc/postfix:
+  file.directory:
+    - user: root
+    - group: root
+    - dir_mode: 755
+    - file_mode: 644
+    - makedirs: True
+`;
+
+    // Act
+    let result = host.getMap(document);
+
+    // Assert
+    t.is(result.declarations[0].functions[0].arguments.length, 5);
+    t.is(result.declarations[0].functions[0].arguments[0].name, "user");
+    t.is(result.declarations[0].functions[0].arguments[0].value, "root");
+    t.is(result.declarations[0].functions[0].arguments[0].startIndex, 39);
+    t.is(result.declarations[0].functions[0].arguments[0].endIndex, 49);
+});
+
